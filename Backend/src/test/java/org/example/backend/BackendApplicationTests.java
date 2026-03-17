3273b9aecee53a044e9024f269630e8b2652772a
planckmax42 <24051208@hdu.edu.cn>
2026-03-17 19:30:48 +0800

    初始化项目

--- Backend/src/test/java/org/example/backend/BackendApplicationTests.java
+++ Backend/src/test/java/org/example/backend/BackendApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package org.example.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Demo1ApplicationTests {
+class BackendApplicationTests {
 
     @Test
     void contextLoads() {
